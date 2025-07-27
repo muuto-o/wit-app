331ffcdc61254d3a2f2b068c954632ead6fd76a6
lucicul <unmnkh@gmail.com>
2025-07-28 03:16:13 +0800

    Added get webhook method

--- index.js
+++ index.js
@@ -14,6 +14,28 @@ const PORT = process.env.PORT || 1337;
 app.get("/", (req, res) => {
   res.status(200).send("You are connected to the chatbot application.");
 });
+app.get("/webhook", (req, res) => {
+  // Your verify token. Should be a string you set.
+  let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
+  // Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Checks the mode and token sent is correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Responds with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
 
 // Creates the endpoint for our webhook
 app.post("/webhook", (req, res) => {
