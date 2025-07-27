64700e19a60bd59567003b7f349ec52b719c3b1c
lucicul <unmnkh@gmail.com>
2025-07-28 02:33:55 +0800

    sdfg

--- index.js
+++ index.js
@@ -69,6 +69,8 @@ function processMessage(message, nlp) {
 
     console.log("trains");
     console.log(traits);
+    console.log("-------------------");
+    console.log(traits["wit$greetings"]);
     if (
       traits["wit$greetings"] &&
       traits["wit$greetings"][0]["value"] === "true"
