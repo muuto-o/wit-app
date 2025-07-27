3f3170440e97c187e6a8f7d1aa4ef963203af090
lucicul <unmnkh@gmail.com>
2025-07-28 01:13:39 +0800

    changed request method to axios

--- service/services.js
+++ service/services.js
@@ -1,34 +1,44 @@
-// Sends response messages via the Send API
-export function callSendAPI(sender_psid, response) {
-  console.log("first line callSendApi");
-  // Construct the message body
-  let request_body = {
+// services.js
+import axios from "axios";
+
+/**
+ * Sends response messages via the Facebook Send API
+ * @param {string} sender_psid - The Page-Scoped ID of the user
+ * @param {object} response - The message object to send
+ */
+export async function callSendAPI(sender_psid, response) {
+  const request_body = {
     messaging_type: "RESPONSE",
     recipient: {
       id: sender_psid,
     },
     message: response,
   };
-
+  console.log("-------------");
+  console.log("-------------");
   console.log("request-body:");
   console.log(request_body);
-  // Send the HTTP request to the Messenger Platform
-  request(
-    {
-      uri: "https://graph.facebook.com/v8.0/me/messages",
-      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-      method: "POST",
-      json: request_body,
-    },
-    (err, res, body) => {
-      if (!err) {
-        console.log("Message sent!");
-      } else {
-        console.log(
-          "----------------------------------------------------ERROR-------------------------------------------------"
-        );
-        console.error("Unable to send message: " + err);
+  console.log("-------------");
+  console.log("-------------");
+  try {
+    const result = await axios.post(
+      `https://graph.facebook.com/v18.0/me/messages`,
+      request_body,
+      {
+        params: {
+          access_token: process.env.PAGE_ACCESS_TOKEN,
+        },
       }
+    );
+
+    console.log("✅ Message sent:", result.data);
+  } catch (error) {
+    console.error("❌ Failed to send message:");
+    if (error.response) {
+      console.error("Status:", error.response.status);
+      console.error("Data:", error.response.data);
+    } else {
+      console.error(error.message);
     }
-  );
+  }
 }
